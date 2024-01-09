c77b9ac5b34fe257dbbe81696e80d4a4b22138f9
mazoal1982 <aloys.manga@gmail.com>
2024-01-09 11:52:14 -0700

    Fixed OR statement in server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const writeFileAsync = util.promisify(fs.writeFile);
 
 // Setting Up Server
 const app = express();
-const PORT = process.env.PORT | 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -78,6 +78,6 @@ res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 // Listening
-app. listen(PORT, function() {
-console.log("App listening on PORT " + PORT);
-})
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
